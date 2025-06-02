32be5bece973b2e8ddcf4b14fa4c0f8bbb94e193
Direwolfesp <alvarolg365@gmail.com>
2025-06-02 19:14:00 +0200

    - [Feature]: Allow restoring all the tabs on exit by saving them into a file, and restoring them on startup by a keybind (it would be great to know how to do it automatically, see discussions/2827)
    - [Minor]: Updated readme, added the new commands and fixed
    the manager keyword (it got updated to mgr)

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A [Yazi](https://github.com/sxyazi/yazi) plugin that adds the functionality to c
 
  - Close the tab, and go to the left/right tab
  - Restore the closed tab to its previous position
+ - Stores and restores all the tabs from previous session
 
 ## Installation
 
@@ -28,20 +29,30 @@ git clone https://github.com/MasouShizuka/close-and-restore-tab.yazi.git ~/.conf
 Add this to your `keymap.toml`:
 
 ```toml
-[[manager.prepend_keymap]]
+[[mgr.prepend_keymap]]
 on = [ "<C-w>" ]
 run = "plugin close-and-restore-tab close_to_left"
 desc = "Close the current tab and turn to left tab, or quit if it is last tab"
 
-[[manager.prepend_keymap]]
+[[mgr.prepend_keymap]]
 on = [ "<C-w>" ]
 run = "plugin close-and-restore-tab close_to_right"
 desc = "Close the current tab and turn to right tab, or quit if it is last tab"
 
-[[manager.prepend_keymap]]
+[[mgr.prepend_keymap]]
 on = [ "<C-t>" ]
 run = "plugin close-and-restore-tab restore"
 desc = "Restore the closed tab"
+
+[[mgr.prepend_keymap]]
+on = [ "q" ]
+run = "plugin close-and-restore-tab store_session"
+desc = "Exits yazi and stores all tabs"
+
+[[mgr.prepend_keymap]]
+on = [ "<C-t>" ]
+run = "plugin close-and-restore-tab restore_session"
+desc = "Restores the tabs from last session"
 ```
 
 `close_to_left` and `close_to_right` respectively mean switching to the left/right tab after closing the tab. Please choose the one you like.
--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+local function fail(s, ...) ya.notify { title = "restore-session.yazi", content = string.format(s, ...), timeout = 4, level = "error" } end
+local function info(s, ...) ya.notify { title = "restore-session.yazi", content = string.format(s, ...), timeout = 2, level = "info" } end
+
 local _get_closed_tabs = ya.sync(function(state)
     return not state.closed_tabs and {} or state.closed_tabs
 end)
@@ -67,6 +70,50 @@ local restore = ya.sync(function(state)
     end
 end)
 
+-- dumps all the tabs into a file
+-- it should be used as a replacement of
+-- regular "quit"
+local quit_and_save = ya.sync(function(state)
+    -- maybe the path could be improved
+    local home = os.getenv("HOME")
+    local file_name = home .. "/.config/yazi/plugins/close-and-restore-tab.yazi/session.txt"
+
+    -- iterate each tab
+    local output = io.open(file_name, "w")
+    if output ~= nil then
+        for i=1, #cx.tabs do
+            local parent = cx.tabs[i].parent.cwd
+            local name = cx.tabs[i].name
+            output:write(parent .. "/" .. name .. "\n")
+        end
+        output:close()
+    else
+        fail("Couldn't store session into '%s'", file_name)
+    end
+    ya.emit("quit", {}) -- exit
+end)
+
+-- Restores and updates the active tabs
+local  restore_session = ya.sync(function(state)
+    local home = os.getenv("HOME")
+    local file_name = home .. "/.config/yazi/plugins/close-and-restore-tab.yazi/session.txt"
+
+    local input = io.open(file_name, "r")
+    if input ~= nil then
+        -- add all the tabs
+        for line in input:lines() do
+            ya.emit("tab_create", {line})
+        end
+
+        -- delete the first one that its always put by yazi
+        ya.emit("tab_close", {0})
+        ya.emit("tab_switch", {0})
+
+        input:close()
+        info("Session restored successfully")
+    end
+end)
+
 return {
     entry = function(_, job)
         local action = job.args[1]
@@ -86,6 +133,16 @@ return {
 
         if action == "restore" then
             restore()
+            return
+        end
+
+        if action == "store_session" then
+            quit_and_save()
+            return
+        end
+
+        if action == "restore_session" then
+            restore_session()
             return
         end
     end,
